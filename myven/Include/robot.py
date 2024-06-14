78009b0422506e33a5afb4365e2aed3de633f759
saadcoder4346 <mr.dictator4346@gmail.com>
2024-06-14 18:13:04 +0500

    Update robot.py

--- myven/Include/robot.py
+++ myven/Include/robot.py
@@ -1,3 +1,4 @@
 import win32com.client as cl
-speaker=cl.Dispatch("sapi.spvoice")
-speaker.speak(input("what do u wnna say:"))
+while True:
+    speaker=cl.Dispatch("sapi.spvoice")
+    speaker.speak(input("what do u wnna say:"))
