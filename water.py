00da1f3d0d5cd342e6ebf0ca970bc70988b57087
saadcoder4346 <159518654+saadcoder4346@users.noreply.github.com>
2024-06-15 17:53:22 +0500

    Add files via upload

---
+++ water.py
@@ -0,0 +1,8 @@
+import time
+from plyer import notification
+
+
+if __name__=="__main__":
+	        notification.notify(title = "DRINK WATER",
+							 message="its been five hours pls drink water",timeout=2)
+time.sleep(18000)
