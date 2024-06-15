e1da8f3b202a1ae10097656e5e3d38e3884fe4bc
saadcoder4346 <159518654+saadcoder4346@users.noreply.github.com>
2024-06-15 17:54:05 +0500

    Update water.py

--- water.py
+++ water.py
@@ -1,7 +1,6 @@
 import time
 from plyer import notification
-
-
+while True:
 if __name__=="__main__":
 	        notification.notify(title = "DRINK WATER",
 							 message="its been five hours pls drink water",timeout=2)
