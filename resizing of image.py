976dfbee3efb33468bbca4593fca4e1a2c36da8a
saadcoder4346 <159518654+saadcoder4346@users.noreply.github.com>
2024-06-15 14:11:46 +0500

    Update resizing of image.py

--- resizing of image.py
+++ resizing of image.py
@@ -6,12 +6,12 @@ image = cv2.imread(r"C:\Users\mrdic\Downloads\random.jpg")
 image.read()
 
 # Define the desired dimensions for the resized image
-# width = 700
-# height = 400
+ width = 700
+ height = 400
 
-# # Resize the image
-# resized_image = cv2.resize(image, (width, height))
+#  Resize the image
+ resized_image = cv2.resize(image, (width, height))
 
-# # Save the resized image
-# cv2.imwrite('resized_image.jpg', resized_image)
-# cv2.waitKey(0)
+#  Save the resized image
+ cv2.imwrite('resized_image.jpg', resized_image)
+ cv2.waitKey(0)
