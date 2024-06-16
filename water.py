8a209ee88b66f89d304dd36862b45d970d77af26
saadcoder4346 <159518654+saadcoder4346@users.noreply.github.com>
2024-06-16 11:42:39 +0500

    Update water.py

--- water.py
+++ water.py
@@ -6,5 +6,5 @@ user_input=input("When do you want us to notify you: ")
 while True:
     t = time.strftime("%I:%M %p")
     if str(t) == str(user_input):
-        notification.notify(title="DRINK WATER", message="its been five hours pls drink water", timeout=2)
+        notification.notify(title="DRINK WATER", message="pls drink water", timeout=2)
         break
