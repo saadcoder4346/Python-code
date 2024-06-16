ff90c6ff6d1a72522a143c2d8de331a5f68fdbd7
saadcoder4346 <159518654+saadcoder4346@users.noreply.github.com>
2024-06-16 14:30:18 +0500

    Add files via upload

---
+++ Rock,paper,sissor.py
@@ -0,0 +1,17 @@
+import random
+lsit=["rock","paper","scissor"]
+user_input=input(f"choose wat u wnna throw:{lsit}\n".lower())
+computer_input=random.choice(lsit)
+if(user_input==computer_input):
+    print("tie")
+elif(user_input=="rock" and computer_input=="scissor"):
+    print("user wins")  
+elif(user_input=="paper" and computer_input=="rock"):
+    print("user wins")  
+elif(user_input=="scissor" and computer_input=="paper"):
+    print("user winss")
+elif user_input not in lsit:
+    print("pls throw somehting valid")    
+else:
+    print("computer wins")           
+#NOTE:even if its a tie the computer will win watchu gnna do bitx    
