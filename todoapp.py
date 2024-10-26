06a9f9c359fb5ebf18514d5300e3928089a64cb2
saadcoder4346 <159518654+saadcoder4346@users.noreply.github.com>
2024-10-26 18:11:40 +0500

    Update todoapp.py
    still in working

--- todoapp.py
+++ todoapp.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,redirect,url_for,request,session
 from flask_sqlalchemy import SQLAlchemy
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app=Flask(__name__,template_folder='templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
@@ -9,29 +9,39 @@ db=SQLAlchemy(app)
 
 class Users(db.Model):
     id=db.Column("id",db.Integer,primary_key=True)
-    username=db.Column(db.String(100))
-    password=db.Column(db.String(100))
+    username=db.Column(db.String(100),unique=True, nullable=False)
+    password=db.Column(db.String(100),nullable=False)
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+
+    user = db.relationship('Users', backref='todos')
+
     
-
-
-
-
-
-
+    
 @app.route("/",methods=["POST",'GET'])
 def hello():
     if request.method=="POST":
         user=request.form['nm']
+        password=request.form['password']
         
         
         founduser=Users.query.filter_by(username=user).first()
         if founduser:
-            session['username']=founduser.username
+            if check_password_hash(founduser.password, password):
+                session['username'] = founduser.username
+                return redirect(url_for('welcome'))
+            else:
+                return "Incorrect password. Please try again."
 
         else:
-         usr=Users(username=user, password="")
+         hashed_password = generate_password_hash(password)
+         usr = Users(username=user, password=hashed_password)
          db.session.add(usr)
          db.session.commit()
+         session['username'] = usr.username
         return redirect(url_for('welcome'))
         
     else: 
